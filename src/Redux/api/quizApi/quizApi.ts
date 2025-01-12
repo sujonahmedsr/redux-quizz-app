13df6f702f561ee2e32d6fd2ac30429c2e1a1052
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-12 16:02:33 +0600

    enpoints creating

--- src/Redux/api/quizApi/quizApi.ts
+++ src/Redux/api/quizApi/quizApi.ts
@@ -1,10 +1,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const quizApi = createApi({
+export const quizApi = createApi({
     reducerPath: "quizApi",
     baseQuery: fetchBaseQuery({baseUrl: `http://localhost:5000/api`}),
     endpoints: (builder) => ({
-        
+        getAllQuiz: builder.query({
+            query: () => `/quizzes`
+        }),
+        addQuiz: builder.mutation({
+            query: (body) => ({
+                url: `/quizzes`,
+                method: "POST",
+                body 
+            })
+        }),
+        updateQuiz: builder.mutation({})
     })
 })
-export default quizApi
+export const {
+    useGetAllQuizQuery
+
+} = quizApi
